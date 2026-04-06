3a04d87ae26631c7ed25839f35cb041a0a8d0abb
A. B. M. Mahmudul Hasan <abmmhasan@users.noreply.github.com>
2026-04-06 20:42:42 +0600

    chores

--- rector.php
+++ rector.php
@@ -6,11 +6,11 @@ use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\SetList;
 
 return static function (RectorConfig $rectorConfig): void {
-    $rectorConfig->paths([__DIR__ . '/src']);
+    $rectorConfig->paths([__DIR__.'/src']);
 
-    $setConstant = SetList::class . '::PHP_' . PHP_MAJOR_VERSION . PHP_MINOR_VERSION;
-    if (!defined($setConstant)) {
-        $setConstant = SetList::class . '::PHP_84';
+    $setConstant = SetList::class.'::PHP_'.PHP_MAJOR_VERSION.PHP_MINOR_VERSION;
+    if (! defined($setConstant)) {
+        $setConstant = SetList::class.'::PHP_84';
     }
 
     $rectorConfig->sets([
